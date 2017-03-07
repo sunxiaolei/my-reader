3d0c269961a1f1227eb78f6b21e69665cccf496a
sunxiaolei <xiaoleisun92@gmail.com>
2017-03-07 16:16:30 +0800

    save img

--- app/src/main/java/sunxl8/my_reader/ui/dbmoment/detail/MomentDetailActivity.java
+++ app/src/main/java/sunxl8/my_reader/ui/dbmoment/detail/MomentDetailActivity.java
@@ -1,9 +1,11 @@
 package sunxl8.my_reader.ui.dbmoment.detail;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.PointF;
 import android.net.Uri;
+import android.os.Environment;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.animation.GlideAnimation;
@@ -11,8 +13,14 @@ import com.bumptech.glide.request.target.SimpleTarget;
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.ImageViewState;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+import com.jakewharton.rxbinding.view.RxView;
+import com.trello.rxlifecycle.android.ActivityEvent;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import butterknife.BindView;
 import sunxl8.my_reader.R;
@@ -30,6 +38,7 @@ public class MomentDetailActivity extends BaseActivity {
     SubsamplingScaleImageView mView;
 
     private PostsBean bean;
+    private File mFile;
 
     @Override
     protected IPresenter createPresenter() {
@@ -55,11 +64,27 @@ public class MomentDetailActivity extends BaseActivity {
             @Override
             public void onResourceReady(File resource, GlideAnimation<? super File> glideAnimation) {
                 dismissDialog();
+                mFile = resource;
                 mView.setImage(ImageSource.uri(Uri.fromFile(resource)), new ImageViewState(2.5F, new PointF(0, 0), 0));
             }
 
-
         });
+
+        RxView.longClicks(mView)
+                .compose(this.bindUntilEvent(ActivityEvent.DESTROY))
+                .subscribe(aVoid -> {
+                    showMenuDialog();
+                });
+    }
+
+    private void showMenuDialog() {
+        new AlertDialog.Builder(this)
+                .setItems(new String[]{"保存到本地", "取消"}, (dialog, which) -> {
+                    if (which == 0) {
+                        saveImage();
+                    }
+                })
+                .show();
     }
 
     @Override
@@ -76,5 +101,27 @@ public class MomentDetailActivity extends BaseActivity {
         Intent intent = new Intent(context, MomentDetailActivity.class);
         intent.putExtra("bean", bean);
         context.startActivity(intent);
+    }
+
+    public void saveImage() {
+        File file = new File(Environment.getExternalStorageDirectory(), "/MyReader/" + System.currentTimeMillis() + ".jpg");
+        file.getParentFile().mkdirs();
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            FileInputStream fis = new FileInputStream(mFile);
+            byte[] buf = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fis.read(buf)) > 0) {
+                fos.write(buf, 0, bytesRead);
+            }
+            fis.close();
+            fos.close();
+            showToast("保存成功");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
